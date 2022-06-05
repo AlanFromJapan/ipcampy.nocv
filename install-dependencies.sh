fae44df295f063bb45e68d1be0fa649e5465d6cf
AlanFromJapan <alanfromjapan@gmail.com>
2022-06-05 14:18:03 +0900

    Fixed install script to run as root for all accounts

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,11 +3,8 @@
 #get python an pip
 sudo apt-get install python3-setuptools python3-pip --yes
 
-#get flask
-sudo python3 -m pip install Flask
-
-pip install av
-pip install pillow
+#get flask and other libs
+sudo pip install flask av pillow
 
 sudo apt-get install libavformat-dev libavdevice-dev --yes
 
