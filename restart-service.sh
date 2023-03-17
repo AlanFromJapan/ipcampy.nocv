f0734a669c3486d080be5207c6c8f709957dd5da
AlanFromJapan <alanfromjapan@gmail.com>
2023-03-17 20:42:23 +0900

    Added logs for restart

--- restart-service.sh
+++ restart-service.sh
@@ -2,5 +2,9 @@
 
 here=`dirname $0`
 
-$here/stop-service.sh
-$here/start-service.sh
+echo Restart initiated at `date` > /tmp/ipcampy_restart_latest.log
+echo ***STOP*** >> /tmp/ipcampy_restart_latest.log
+$here/stop-service.sh >> /tmp/ipcampy_restart_latest.log 2>&1
+echo ***START*** >> /tmp/ipcampy_restart_latest.log
+$here/start-service.sh >> /tmp/ipcampy_restart_latest.log 2>&1
+echo Restart completed at `date` >> /tmp/ipcampy_restart_latest.log
