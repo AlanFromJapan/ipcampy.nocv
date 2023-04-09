e70bcfd52fc792907f864112c33560120aef5ba6
AlanFromJapan <alanfromjapan@gmail.com>
2023-04-09 23:01:16 +0900

    cam have high/low res settings, rollback mistakenly commited tests on camcapture

--- sharedObjects.py
+++ sharedObjects.py
@@ -4,19 +4,33 @@ class IpCamera:
     port = ""
     login = ""
     password = ""
-    suffix = ""
+    suffixLowRes = ""
+    suffixHighRes = ""
     #see captureStill() for details
     overlay_label = ""
 
-    def __init__(self, nickname, ip, port, login, password, suffix, lbl="Captured at {0:%Y/%m/%d %H:%M:%S}") -> None:
+    #Constants a la Python, the default label for overlay
+    def defaultLabel():
+        return "Captured at {0:%Y/%m/%d %H:%M:%S}"
+
+
+    def __init__(self, nickname, ip, port, login, password, suffixLowRes, suffixHighRes=suffixLowRes, lbl=defaultLabel()) -> None:
         self.ip = ip
-        self.suffix = suffix
+        self.suffixLowRes = suffixLowRes
+        self.suffixHighRes = suffixHighRes
         self.port = port
         self.login = login
         self.password = password
         self.nickname = nickname
         self.overlay_label = lbl
 
+    #get the connection URL
+    def url(self, highRes=False):
+        suffix = self.suffixHighRes if highRes else self.suffixLowRes
 
-    def url(self):
-        return f"rtsp://{self.ip}:{self.port}{self.suffix}"
+        if self.login and not self.login.isspace():
+            #there's a login non null non empty
+            return f"rtsp://{self.login}:{self.password}@{self.ip}:{self.port}{suffix}"
+        #anonymous string
+        return f"rtsp://{self.ip}:{self.port}{suffix}"
+    
